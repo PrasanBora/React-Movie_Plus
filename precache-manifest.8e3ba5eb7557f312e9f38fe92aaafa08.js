85e45c198ef63796506ebb63115152af23b21f51
PrasanBora <prasanbora23@gmail.com>
2023-05-04 11:48:49 +0530

    Updates

---
+++ precache-manifest.8e3ba5eb7557f312e9f38fe92aaafa08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "072d63e6c694e783f3ff",
+    "url": "/React-Movie_Plus/static/css/main.e74557b0.chunk.css"
+  },
+  {
+    "revision": "072d63e6c694e783f3ff",
+    "url": "/React-Movie_Plus/static/js/main.072d63e6.chunk.js"
+  },
+  {
+    "revision": "4ff0c5bf416a62f1f39c",
+    "url": "/React-Movie_Plus/static/js/1.4ff0c5bf.chunk.js"
+  },
+  {
+    "revision": "47b17deeffdbbf337321",
+    "url": "/React-Movie_Plus/static/js/runtime~main.47b17dee.js"
+  },
+  {
+    "revision": "876223b834c2c50042d363d805e17708",
+    "url": "/React-Movie_Plus/static/media/search.876223b8.svg"
+  },
+  {
+    "revision": "0ecd3a0935b6e702687435b3973c0476",
+    "url": "/React-Movie_Plus/index.html"
+  }
+];
